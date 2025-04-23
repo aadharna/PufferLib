44cc0eb83b1dc8d0deb9012929c14fb4dde25583
aadharna <aadharna@gmail.com>
2025-04-23 21:30:01 +0000

    update to new dev

--- clean_pufferl.py
+++ clean_pufferl.py
@@ -324,6 +324,10 @@ def evaluate(data):
     data.ep_indices = torch.arange(data.total_agents, device=config.device, dtype=torch.int32)
     data.ep_lengths.zero_()
     data.ep_uses.zero_()
+    try:
+        data.vecenv.notify()
+    except:
+        pass
     profile.end()
     return data.stats, infos
 
--- pufferlib/environments/metta/environment.py
+++ pufferlib/environments/metta/environment.py
@@ -1,8 +1,12 @@
+from pdb import set_trace as T
 import functools
+
+import numpy as np
 
 import pufferlib
 from mettagrid.gym_wrapper import RaylibRendererWrapper
 
+from pufferlib.learning_progress import BidirectionalLearningProgess
 
 def env_creator(name='metta'):
     return functools.partial(make, name)
@@ -12,10 +16,21 @@ def make(name, config='pufferlib/environments/metta/metta.yaml', render_mode='au
     return MettaPuff(config, render_mode, buf)
 
 class MettaPuff(pufferlib.PufferEnv):
-    def __init__(self, config, render_mode='human', buf=None, seed=0):
+    def __init__(self, config, render_mode='human', buf=None, seed=0, num_maps = 64, ema_alpha = 0.001, p_theta = 0.05, num_active_tasks = 16, rand_task_rate = 0.2, 
+                 sample_threshold = 15, memory = 25):
         self.render_mode = render_mode
+        self.n = num_maps
         import mettagrid.mettagrid_env
         self.env = mettagrid.mettagrid_env.make_env_from_cfg(config, render_mode, buf=buf)
+
+        self.cfgs = [self.env._get_new_env_cfg() for _ in range(self.n)]
+        self.levels = np.arange(self.n)
+
+        # T()
+        self.lp = BidirectionalLearningProgess(self.n, ema_alpha, p_theta, 
+                                               num_active_tasks, rand_task_rate, 
+                                               sample_threshold, memory) 
+        self.send_lp_metrics = False
 
         if render_mode == 'human':
             from mettagrid.gym_wrapper import RaylibRendererWrapper
@@ -36,6 +51,22 @@ class MettaPuff(pufferlib.PufferEnv):
         obs, rew, term, trunc, info = self.env.step(actions)
 
         if all(term) or all(trunc):
+            self.lp.collect_data({f'tasks/{self._env_cfg_idx}': [info["episode/reward.mean"]]})
+            info[f'{self._env_cfg_idx}/reward.mean'] = info["episode/reward.mean"]
+            info['num_active_tasks'] = len(self.levels)
+            self.should_reset = True
+            if self.send_lp_metrics:
+                info['lp/mean_sample_prob'] = np.mean(self.lp_dist)
+                info['lp/num_zeros_lp_dist'] = np.sum(self.lp_dist == 0)
+                info['lp/task_1_success_rate'] = self.lp.task_success_rate[0]
+                info['lp/task_500_success_rate'] = self.lp.task_success_rate[499]
+                info['lp/last_task_success_rate'] = self.lp.task_success_rate[-1]
+                info['lp/task_success_rate'] = np.mean(self.lp.task_success_rate)
+                info['lp/mean_evals_per_task'] = self.lp.mean_samples_per_eval[-1]
+                info['lp/num_nan_tasks'] = self.lp.num_nans[-1]
+                # for l in self.levels:
+                #     infos[f'lp/task_{l}_samples'] = len(self.lp.outcomes[l])
+                # self.send_lp_metrics = False
             self.reset()
             if 'agent_raw' in info:
                 del info['agent_raw']
@@ -47,12 +78,33 @@ class MettaPuff(pufferlib.PufferEnv):
         return obs, rew, term, trunc, [info]
 
     def reset(self, seed=None):
-        obs = self.env.reset()
+        self._env_cfg_idx = np.random.choice(self.levels)
+        self.env._env_cfg = self.cfgs[self._env_cfg_idx]
+        self.env._reset_env()
+
+        self.env._c_env.set_buffers(
+            self.env.observations,
+            self.env.terminals,
+            self.env.truncations,
+            self.env.rewards)
+
+        # obs, infos = self._env.reset(**kwargs)
+        # return obs, infos
+        obs, infos = self.env._c_env.reset()
+        self.should_reset = False
         self.tick = 0
-        return obs, []
+        return obs, infos
+        # obs = self.env.reset()
+        # self.tick = 0
+        # return obs, []
 
     def render(self):
         self.env.render()
 
     def close(self):
         self.env.close()
+
+    def notify(self):
+        self.sampling_dist, self.levels = self.lp.calculate_dist()
+        self.lp_dist = self.sampling_dist
+        self.send_lp_metrics = True
